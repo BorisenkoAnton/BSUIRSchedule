79429665b954934fed6c8781e70ad4fa2344b6a3
BorisenkoAnton <ant.borisenko2013@gmail.com>
2020-07-11 22:24:50 +0300

    added main screen table view

---
+++ BSUIR Schedule/BSUIR Schedule/ScheduleTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  ScheduleTableViewCell.swift
+//  BSUIR Schedule
+//
+//  Created by Anton Borisenko on 7/11/20.
+//  Copyright © 2020 Anton Borisenko. All rights reserved.
+//
+
+import UIKit
+
+class ScheduleTableViewCell: UITableViewCell {
+
+    @IBOutlet weak var professorsPhoto: UIImageView! {
+        didSet {
+            // to make image view to be circle
+            professorsPhoto.layer.cornerRadius = professorsPhoto.frame.size.height / 2
+            professorsPhoto.clipsToBounds = true
+        }
+    }
+    @IBOutlet weak var subjectNameLabel: UILabel!
+    @IBOutlet weak var subjectTypeLabel: UILabel!
+    @IBOutlet weak var subjectTimeLabel: UILabel!
+    @IBOutlet weak var subjectAuditoryLabel: UILabel!
+}
