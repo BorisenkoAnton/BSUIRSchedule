2c2f4b2153889999fe261072eaddc3672b40e42c
BorisenkoAnton <ant.borisenko2013@gmail.com>
2020-07-11 22:26:48 +0300

    added class for managing alamofire requests

---
+++ BSUIR Schedule/BSUIR Schedule/AlamofireNetworkRequest.swift
@@ -0,0 +1,32 @@
+//
+//  AlamofireNetworkRequest.swift
+//  BSUIR Schedule
+//
+//  Created by Anton Borisenko on 7/11/20.
+//  Copyright © 2020 Anton Borisenko. All rights reserved.
+//
+
+import Foundation
+import Alamofire
+
+class AlamofireNetworkRequest {
+    
+    static func sendGetRequestForString(url: String, completion: @escaping (_ stringRepresentationOfData: String) -> ()) {
+        
+        guard let url = URL(string: url) else { return }
+        
+        AF.request(url, method: .get).validate().responseString { (response) in
+            
+            switch response.result {
+            case .success(let value):
+                completion(value)
+            case .failure(let error):
+                // TODO: add showAlert()
+                print(error)
+            }
+        }
+    }
+    
+    // TODO: add sendGetRequestForJSON
+    
+}
