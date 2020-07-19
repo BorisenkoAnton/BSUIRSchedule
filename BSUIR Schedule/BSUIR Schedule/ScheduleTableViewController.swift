c4fba88793af8b5629ef6e8b0a4b5bfd64960052
BorisenkoAnton <ant.borisenko2013@gmail.com>
2020-07-20 00:07:30 +0300

    changed data source funcs

--- BSUIR Schedule/BSUIR Schedule/ScheduleTableViewController.swift
+++ BSUIR Schedule/BSUIR Schedule/ScheduleTableViewController.swift
@@ -8,29 +8,65 @@
 
 import UIKit
 import Alamofire
+import RealmSwift
 
 class ScheduleTableViewController: UITableViewController {
-
+    
+    private var savedSchedules: Results<DBmodel>!
+    private var schedules = [Schedule]()
+    private var currentWeekNumber = 1
+    private var currentWeekDay = 0
+    private var lessons = [LessonInfo]()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        NetworkManager.getCurrentWeekNumber() { (response) in
+            self.currentWeekNumber = response
+        
+            self.currentWeekDay = ScheduleManager.getCurrentWeekDay()
+            
+            self.savedSchedules = realm.objects(DBmodel.self)
+            for savedSchedule: DBmodel in self.savedSchedules {
 
-        NetworkManager.getScheduleForGroup("751004")
+                let json = Data(savedSchedule.jsonRepresentationOfSchedule.utf8)
+                do {
+                    let schedule = try JSONDecoder().decode(Schedule.self, from: json)
+                    self.schedules.append(schedule)
+                } catch {
+                    print(error)
+                }
+            }
+            
+            self.lessons = ScheduleManager.getCurrentDaySchedule(weekNumber: self.currentWeekNumber, weekDay: self.currentWeekDay, schedule: self.schedules[0])
+            
+            self.tableView.reloadData()
+        }
+        
     }
 
     // MARK: - Table view data source
 
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 1
+        return lessons.count
     }
 
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         
         let cell = tableView.dequeueReusableCell(withIdentifier: "ScheduleCell", for: indexPath) as! ScheduleTableViewCell
-
-        cell.subjectNameLabel.text = "History"
-        cell.subjectTypeLabel.text = "ЛР"
-        cell.subjectTimeLabel.text = "15:20 - 16:55"
-        cell.subjectAuditoryLabel.text = "422a - 4"
+        
+        guard !lessons.isEmpty else { return UITableViewCell() }
+        
+        let lesson = lessons[indexPath.row]
+        
+        cell.professorNameLabel.text = lesson.professorName
+        cell.subjectNameLabel.text = lesson.subjectName
+        cell.subjectTypeLabel.text = lesson.lessonType
+        cell.subjectTimeLabel.text = lesson.lessonTime
+        cell.subjectAuditoryLabel.text = lesson.auditorium
+        if lesson.professorsPhoto != nil {
+            cell.professorsPhoto.image = lesson.professorsPhoto
+        }
         
         return cell
     }
