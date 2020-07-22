72f409cfc0f1ab0cf286608e42168ae363b6c217
BorisenkoAnton <ant.borisenko2013@gmail.com>
2020-07-22 23:13:54 +0300

    added func to get all group numbers and changed getScheduleForGroup function structure (added closure)

--- BSUIR Schedule/BSUIR Schedule/NetworkManager.swift
+++ BSUIR Schedule/BSUIR Schedule/NetworkManager.swift
@@ -10,13 +10,13 @@ import Foundation
 
 class NetworkManager {
     
-    static func getScheduleForGroup(_ groupNumber: String) {
+    static func getScheduleForGroup(_ groupNumber: String, completion: @escaping (DBmodel) -> ()) {
         
         let url =  "https://journal.bsuir.by/api/v1/studentGroup/schedule?studentGroup=" + groupNumber
         AlamofireNetworkRequest.sendGetRequestForString(url: url) { (stringRepresentationOfSchedule) in
             if let schedule = DBmodel(jsonRepresentationOfSchedule: stringRepresentationOfSchedule, groupNumber: Int(groupNumber)!) {
                 StorageManager.saveObject(schedule)
-                print(stringRepresentationOfSchedule)
+                completion(schedule)
             }
         }
     }
@@ -27,6 +27,30 @@ class NetworkManager {
         AlamofireNetworkRequest.sendGetRequestForString(url: url) { (currentWeekNumberString) in
             guard let currentWeekNumber = Int(currentWeekNumberString) else { return }
             completion(currentWeekNumber)
+        }
+    }
+    
+    static func getAllGroupNumbers(completion: @escaping (_ groupNumbers: [String]) -> ()) {
+        
+        let url = "https://journal.bsuir.by/api/v1/groups"
+        
+        AlamofireNetworkRequest.sendGetRequestForData(url: url) { (json) in
+            
+            var groupNumbers = [String]()
+            
+            do {
+                let groups = try JSONDecoder().decode([Schedule.StudentGroup].self, from: json)
+                
+                for group in groups {
+                    groupNumbers.append(group.name)
+                }
+                
+                groupNumbers.sort()
+            } catch {
+                // TODO: insert alert here
+            }
+            
+            completion(groupNumbers)
         }
     }
 }
