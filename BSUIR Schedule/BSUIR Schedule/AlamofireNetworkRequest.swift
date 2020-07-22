fb04fa48c246ba25c82192d2a88749ae066610a3
BorisenkoAnton <ant.borisenko2013@gmail.com>
2020-07-22 23:13:09 +0300

    added functions to get json and Data

--- BSUIR Schedule/BSUIR Schedule/AlamofireNetworkRequest.swift
+++ BSUIR Schedule/BSUIR Schedule/AlamofireNetworkRequest.swift
@@ -28,6 +28,35 @@ class AlamofireNetworkRequest {
         
     }
     
-    // TODO: add sendGetRequestForJSON
+    static func sendGetRequestForJSON(url: String, completion: @escaping (Any) -> ()) {
+        
+        guard let url = URL(string: url) else { return }
+        
+        AF.request(url, method: .get, encoding: JSONEncoding.default).validate().responseJSON { (response) in
+            
+            switch response.result {
+            case .success(let value):
+                completion(value)
+            case .failure(let error):
+                // TODO: add showAlert()
+                print(error)
+            }
+        }
+    }
     
+    static func sendGetRequestForData(url: String, completion: @escaping (Data) -> ()) {
+        
+        guard let url = URL(string: url) else { return }
+        
+        AF.request(url, method: .get, encoding: JSONEncoding.default).validate().responseData { (response) in
+            
+            switch response.result {
+            case .success(let value):
+                completion(value)
+            case .failure(let error):
+                // TODO: add showAlert()
+                print(error)
+            }
+        }
+    }
 }
